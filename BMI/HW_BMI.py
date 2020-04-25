24a2dae98f2eaeb4a80b85c9fd188db8d5eedc10
Пользователь <Sergel-A@mail.ru>
2020-04-25 12:09:02 +0300

    'BMI_last_version'

--- BMI/HW_BMI.py
+++ BMI/HW_BMI.py
@@ -1,68 +1,69 @@
-def start():
-    print('\n')
-    print('Калькулятор индекса массы тела (ИМТ/BMI)' + '\n')
+print('\n')
+print('Калькулятор индекса массы тела (ИМТ/BMI)' + '\n')
 
-    while True:
-        try:
-            weight = float(input('Ваш вес (кг.): '))
-        except ValueError:
-            print('Введите число')
-        else:
-            break
-
-    while True:
-        try:
-            height = float(input('Ваш рост (м.): '))
-        except ValueError:
-            print('Введите число')
-        else:
-            break
-
-    bmi = weight / (height * height)
-    print('Ваш ИМТ составляет: ', bmi)
-
-    ibmi = int(bmi)
-
-    if ibmi > 100:
-        print('Проверьте введенные данные')
-        return
-    else: 
-        pass
-
-    if ibmi < 16:
-        print('Выраженный дефицит массы тела')
-    else: 
-        pass
-
-    if ibmi >= 16 and bmi < 18.5:
-        print('Недостаточная масса тела')
+while True:
+    try:
+        weight = float(input('Ваш вес (кг.): '))
+    except ValueError:
+        print('Введите число')
     else:
-        pass
+        break
 
-    if ibmi >= 18.5 and bmi < 25:
-        print('Нормальная масса тела')
+while True:
+    try:
+        height = float(input('Ваш рост (м.): '))
+    except ValueError:
+        print('Введите число')
     else:
-        pass
+        break
 
-    if ibmi >= 25 and bmi < 30:
-        print('Избыточная масса тела (предожирение)')
-    else:
-        pass
+bmi = weight / (height * height)
+print('Ваш ИМТ составляет: ', bmi)
 
-    if ibmi >= 30 and bmi < 35:
-        print('Ожирение 1-ой степени')
-    else:
-        pass
+ibmi = int(bmi)
 
-    if ibmi >= 35 and bmi < 40:
-        print('Ожирение 2-ой степени')
-    else:
-        pass
+if ibmi > 100:
+    print('Проверьте введенные данные')
+    exit
+else: 
+    pass
 
-    if ibmi >= 40:
-        print('Ожирение 3-ой степени')
-    else:
-        pass
+if ibmi < 16:
+    print('Выраженный дефицит массы тела')
+else: 
+    pass
 
+if ibmi >= 16 and ibmi < 18.5:
+    print('Недостаточная масса тела')
+else:
+    pass
+
+if ibmi >= 18.5 and ibmi < 25:
+    print('Нормальная масса тела')
+else:
+    pass
+
+if ibmi >= 25 and ibmi < 30:
+    print('Избыточная масса тела (предожирение)')
+else:
+    pass
+
+if ibmi >= 30 and ibmi < 35:
+    print('Ожирение 1-ой степени')
+else:
+    pass
+
+if ibmi >= 35 and ibmi < 40:
+    print('Ожирение 2-ой степени')
+else:
+    pass
+
+if ibmi >= 40 and ibmi <100:
+    print('Ожирение 3-ой степени')
+else:
+    pass
+
+if ibmi <100:
     print(str('0') + ('=' * int(ibmi - 1)) + '|' + ('=' * int(99 - ibmi)) + str('100'))
-print(start)
+else:
+    exit
